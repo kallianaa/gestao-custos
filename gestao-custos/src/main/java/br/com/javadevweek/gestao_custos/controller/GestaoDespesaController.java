d0d059d1504e8f433eba7e7119574985debc6740
kall!ana <kalliana.glaeser@gmail.com>
2026-07-25 20:58:38 -0300

    praticnado spring boot

---
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/controller/GestaoDespesaController.java
@@ -0,0 +1,13 @@
+package br.com.javadevweek.gestao_custos.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/gestao")
+public class GestaoDespesaController {
+
+    @PostMapping("/create")
+    public void create(){
+        
+    }
+}
---
+++ gestao-custos/src/test/java/br/com/javadevweek/gestao_custos/GestaoCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javadevweek.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
