772711c5e521c9229d62d6025e0d62b38eb4e298
kall!ana <kalliana.glaeser@gmail.com>
2026-07-26 18:30:47 -0300

    Update GestaoDespesaController.java

--- gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/controller/GestaoDespesaController.java
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/controller/GestaoDespesaController.java
@@ -1,13 +1,25 @@
 package br.com.javadevweek.gestao_custos.controller;
 
+import br.com.javadevweek.gestao_custos.enitity.Despesas;
+import br.com.javadevweek.gestao_custos.useCases.CadastroDespesaUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/gestao")
 public class GestaoDespesaController {
 
+    //cadastro de despesas
+    //criar tabela no banco de dados
+    //criar entidade
+
+    @Autowired
+    CadastroDespesaUseCase cadastroDespesaUseCase;
+
     @PostMapping("/create")
-    public void create(){
-        
+    public void create(@RequestBody Despesas despesas){
+
+        cadastroDespesaUseCase.execute(despesas);
     }
 }
