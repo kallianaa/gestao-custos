65105b90c2d94e1e0869416331d3b4ee6473a084
kall!ana <kalliana.glaeser@gmail.com>
2026-08-01 10:35:08 -0300

    Update CadastroDespesaUseCase.java

--- gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
@@ -6,23 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-class CadastroDespesaUseCaseService {
-    
-}
 public class CadastroDespesaUseCase {
-    //SOLID
-    //Single Responsability Principle
+    // SOLID
+    // Single Responsability Principle
 
     @Autowired
     private DespesaRepository despesaRepository;
 
-    public Despesas execute(Despesas despesas){
+    public Despesas execute(Despesas despesas) {
 
-        if(despesas.getCategoria() == null || despesas.getData()== null || despesas.getDescricao() == null ||
-                despesas.getEmail() == null){
+        if (despesas.getCategoria() == null || despesas.getData() == null ||
+                despesas.getDescricao() == null || despesas.getEmail() == null) {
             throw new IllegalArgumentException("Campos obrigatórios não preenchidos");
-
         }
+
         despesas = despesaRepository.save(despesas);
         return despesas;
 
