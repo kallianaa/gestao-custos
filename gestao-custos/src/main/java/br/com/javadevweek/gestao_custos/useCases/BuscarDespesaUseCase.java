d8373414aa1783ef012a69336c09359c4f00458d
kall!ana <kalliana.glaeser@gmail.com>
2026-08-01 10:35:05 -0300

    Create BuscarDespesaUseCase.java

---
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/useCases/BuscarDespesaUseCase.java
@@ -0,0 +1,29 @@
+package br.com.javadevweek.gestao_custos.useCases;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.javadevweek.gestao_custos.enitity.Despesas;
+import br.com.javadevweek.gestao_custos.repository.DespesaRepository;
+
+@Service
+public class BuscarDespesaUseCase {
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    public List<Despesas> execute(String email, LocalDate data){
+        List<Despesas> despesas;
+        if (data != null) {
+            despesas = despesaRepository.findByEmailAndData(email, data);
+            
+        } else {
+            despesas = despesaRepository.findByEmail(email);
+        }
+        return despesas;
+    }
+    
+}
