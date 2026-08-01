18fb437cff39528196f159b48fa0ea0fe09f6ee7
kall!ana <kalliana.glaeser@gmail.com>
2026-08-01 10:35:01 -0300

    Update DespesaRepository.java

--- gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/repository/DespesaRepository.java
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/repository/DespesaRepository.java
@@ -1,10 +1,21 @@
 package br.com.javadevweek.gestao_custos.repository;
 
 import br.com.javadevweek.gestao_custos.enitity.Despesas;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface DespesaRepository extends JpaRepository <Despesas, UUID>{
+
+    List<Despesas> findByEmail(String email);
+
+    List<Despesas> findByEmailAndData(String email, LocalDate data);
+
+    Page<Despesas> findByEmail(String email, Pageable pageable);
 
 }
