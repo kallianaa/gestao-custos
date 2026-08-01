f98d3c923f81bc3d5f548c6dfbfe834e61ff0177
kall!ana <kalliana.glaeser@gmail.com>
2026-08-01 10:34:46 -0300

    Update GestaoDespesaController.java

--- gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/controller/GestaoDespesaController.java
+++ gestao-custos/src/main/java/br/com/javadevweek/gestao_custos/controller/GestaoDespesaController.java
@@ -1,25 +1,53 @@
 package br.com.javadevweek.gestao_custos.controller;
 
+import br.com.javadevweek.gestao_custos.custom_messages.ErrorMessage;
 import br.com.javadevweek.gestao_custos.enitity.Despesas;
+import br.com.javadevweek.gestao_custos.useCases.BuscarDespesaUseCase;
 import br.com.javadevweek.gestao_custos.useCases.CadastroDespesaUseCase;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/gestao")
 public class GestaoDespesaController {
 
-    //cadastro de despesas
-    //criar tabela no banco de dados
-    //criar entidade
+    // cadastro de despesas
+    // criar tabela no banco de dados
+    // criar entidade
 
     @Autowired
     CadastroDespesaUseCase cadastroDespesaUseCase;
 
-    @PostMapping("/create")
-    public Despesas create(@RequestBody Despesas despesas){
+    @Autowired
+    BuscarDespesaUseCase buscarDespesaUseCase;
 
-        return cadastroDespesaUseCase.execute(despesas);
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Despesas despesas) {
+
+        try {
+            var result = cadastroDespesaUseCase.execute(despesas);
+            return ResponseEntity.ok(result);
+        } catch (IllegalArgumentException e) {
+            var errorMessage = new ErrorMessage(e.getMessage(), "INVALID_PARAMS");
+            return ResponseEntity.status(400).body(errorMessage);
+        }
+    }
+
+    // /gestao/find/INSERIR EMAIL AQUI?data=2026-07
+    @GetMapping("/{email}")
+    public List<Despesas> findyByEmailAndDate(@PathVariable String email,
+            @RequestParam(required = false) LocalDate data) {
+        return buscarDespesaUseCase.execute(email, data);
     }
 }
